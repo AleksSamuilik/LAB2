f1c1414ab4ae064d1bee600a0f71a6d270f672fd
Spawn <alkspawn@gmail.com>
2019-03-13 18:10:13 +0300

    reformat code

--- Factorial.java
+++ Factorial.java
@@ -2,35 +2,33 @@ import java.util.Scanner;
 
 public class Factorial {
     public static void main(String[] args) {
-        int number = 0;
-        System.out.println("Данная программа находит факториал необходимого вам числа.");
+
+        System.out.println("This program finds the factorial of the number");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите число:");
+        System.out.println("Write number:");
 
         if (scanner.hasNextLine()) {
-            String chislo = scanner.nextLine();
+            String numberString = scanner.nextLine();
             try {
-                number = Integer.parseInt(chislo);
-                if (number > 0) {
+                int numberInt = Integer.parseInt(numberString);
+                if (numberInt >= 0) {
                     scanner.close();
-                    System.out.println(factorial(number));
+                    System.out.println(factorial(numberInt));
                 } else {
-                    System.out.println("Извините, введите положительное число. Перезапустите программу и попробуйте снова!");
+                    scanner.close();
+                    System.out.println("Sorry, write a positive number. Restart the program and try again.");
                 }
             } catch (NumberFormatException e) {
-                System.out.println("Извините, но это явно не число. Перезапустите программу и попробуйте снова!");
+                scanner.close();
+                System.out.println("Sorry, but this is not a number. Restart the program and try again!");
                 return;
             }
         }
-
-
     }
 
     private static int factorial(int n) {
         int result = 1;
-        if (n == 1 || n == 0) {
-            return result;
-        }
+        if (n == 1 || n == 0) return result;
         result = n * factorial(n - 1);
         return result;
     }
